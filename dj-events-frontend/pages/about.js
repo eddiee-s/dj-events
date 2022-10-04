c91a017b7a36ddfc8d90498780186f60d4731f41
eddiee-s <edis.pajaziti@gmail.com>
2022-10-04 07:48:40 +0200

    pages added

---
+++ dj-events-frontend/pages/about.js
@@ -0,0 +1,13 @@
+
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>AboutPage</h1>
+      <p> App to find latest dj and other musical events </p>
+      <p>Version: 1.0.0</p>
+    </div>
+  )
+}
+
+export default AboutPage
