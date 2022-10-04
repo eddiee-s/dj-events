8ec2195dad873dfa7c1a5b5a7183a86b43177a91
eddiee-s <edis.pajaziti@gmail.com>
2022-10-04 09:05:50 +0200

    head and layout config

--- dj-events-frontend/pages/about.js
+++ dj-events-frontend/pages/about.js
@@ -1,12 +1,13 @@
+import Layout from "../.next/components/Layout"
 
 
 const AboutPage = () => {
   return (
-    <div>
+    <Layout title='About DJ Events'>
       <h1>AboutPage</h1>
       <p> App to find latest dj and other musical events </p>
       <p>Version: 1.0.0</p>
-    </div>
+    </Layout>
   )
 }
 
