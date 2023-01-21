7adf815f77f4e5e4d3217c0d7fd3ce97378cefd5
eddiee-s <edis.pajaziti@gmail.com>
2023-01-21 16:09:55 +0100

    initial settings

--- dj-events-frontend/components/Layout.js
+++ dj-events-frontend/components/Layout.js
@@ -3,14 +3,14 @@ import styles from "../styles/Layout.module.css"
 
 const Layout = ({ title, keywords, description, children}) => {
   return (
-    <div>
+    <>
         <Head>
             <title> {title} </title>
             <meta name="description" content={description}/>
             <meta name="keywords" content={keywords}/>
         </Head>
         <div className={styles.container}>{ children }</div>
-    </div>
+    </>
   )
 }
 
