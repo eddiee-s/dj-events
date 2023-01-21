c92674634dc32b5526ce4b69903875ce2dc5a822
eddiee-s <edis.pajaziti@gmail.com>
2023-01-22 00:45:08 +0100

    singleEvent with getStaticPaths  done

--- dj-events-frontend/pages/events/[slug].js
+++ dj-events-frontend/pages/events/[slug].js
@@ -1,12 +1,49 @@
-import Layout from "@/components/Layout"
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-const EventPage = () => {
-
+const EventPage = ({ evt }) => {
   return (
     <Layout>
-        <h1>My Event</h1>
+      <h1>{evt.name}</h1>
     </Layout>
-  )
+  );
+};
+
+export default EventPage;
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
 }
 
-export default EventPage
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  };
+}
+
+//getServerSideProps changes getStaticProps and getStaticPaths
+//getStaticProps and getStaticPaths goes together for statis sites
+
+// export async function getServerSideProps ({query:{slug}}) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+//   const events = await res.json()
+//   return {
+//     props:{
+//       evt: events[0]
+//     }
+//   }
+// }
