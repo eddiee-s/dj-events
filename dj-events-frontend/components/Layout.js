937e807309c100b963bc4f2bc6b483b1e1f298f1
eddiee-s <edis.pajaziti@gmail.com>
2023-01-21 16:34:32 +0100

    Layout, Header and Footer

--- dj-events-frontend/components/Layout.js
+++ dj-events-frontend/components/Layout.js
@@ -1,25 +1,27 @@
-import Head from "next/head"
-import styles from "../styles/Layout.module.css"
-import Header from "./Header"
+import Head from "next/head";
+import styles from "../styles/Layout.module.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
-const Layout = ({ title, keywords, description, children}) => {
+const Layout = ({ title, keywords, description, children }) => {
   return (
     <>
-        <Head>
-            <title> {title} </title>
-            <meta name="description" content={description}/>
-            <meta name="keywords" content={keywords}/>
-        </Head>
-        <Header/>
-        <div className={styles.container}>{ children }</div>
+      <Head>
+        <title> {title} </title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+      <Header />
+      <div className={styles.container}>{children}</div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
 
 Layout.defaultProps = {
-    title: "DJ EVENTS",
-    description: "Hotest DJ Events and parties",
-    keywords: "event, party, parties, edm",
-}
+  title: "DJ EVENTS",
+  description: "Hotest DJ Events and parties",
+  keywords: "event, party, parties, edm",
+};
